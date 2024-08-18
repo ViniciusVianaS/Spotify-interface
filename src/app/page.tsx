3effe3977a714799f7d8d42a0af2e9477854ff9f
Vinicius Viana <viniciuspro333@gmail.com>
2024-08-18 13:28:43 -0300

    feat: Creating responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2} from "lucide-react"
+import {
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
 
 import { Sidebar } from "@/components/Sidebar";
 import { Footer } from "@/components/Footer";
@@ -7,7 +11,9 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <Sidebar />
+        <div className="hidden md:block">
+          <Sidebar />
+        </div>
         <main className="flex-1 p-6">
           <div className="flex items-center gap-3">
             <button className="rounded-full bg-black/40 p-1">
@@ -18,81 +24,164 @@ export default function Home() {
             </button>
           </div>
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
-          <div className="grid grid-cols-3 gap-4 mt-4">
-            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <img src="/justin.png" width={70} height={70}  alt="Album image"/>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <img src="/justin.png" width={70} height={70} alt="Album image" />
               <strong>This is Justin Bieber</strong>
               <button className="w-11 h-11 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="bg-black" />
               </button>
             </a>
-            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <img src="/eminem.jpg" width={70} height={70}  alt="Album image"/>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <img src="/eminem.jpg" width={70} height={70} alt="Album image" />
               <strong>This is Eminem</strong>
               <button className="w-11 h-11 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="bg-black" />
               </button>
             </a>
-            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <img src="/tweeknd.jpg" width={70} height={70}  alt="Album image"/>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <img
+                src="/tweeknd.jpg"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
               <strong>This is The Weeknd</strong>
               <button className="w-11 h-11 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="bg-black" />
               </button>
             </a>
-            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <img src="/matue.webp" width={70} height={70}  alt="Album image"/>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <img src="/matue.webp" width={70} height={70} alt="Album image" />
               <strong>This is Matuê</strong>
               <button className="w-11 h-11 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="bg-black" />
               </button>
             </a>
-            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <img src="/brazil.jpg" width={70} height={70}  alt="Album image"/>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <img src="/brazil.jpg" width={70} height={70} alt="Album image" />
               <strong>Top 50 - Brasil</strong>
               <button className="w-11 h-11 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="bg-black" />
               </button>
             </a>
-            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <img src="/eletronica.jpg" width={70} height={70}  alt="Album image"/>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <img
+                src="/eletronica.jpg"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
               <strong>Eletronica</strong>
               <button className="w-11 h-11 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="bg-black" />
               </button>
             </a>
           </div>
-          <h2 className="font-semibold text-2xl mt-10">Made for Vinicius Viana</h2>
-          <div className="grid grid-cols-6 gap-4 mt-4">
-            <a href="#" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
-              <img src="/matue.webp" className="w-full" width={70} height={70}  alt="Album image"/>
-              <strong className="font-semibold ">Daily mix 1</strong>
-              <span className="Text-sm text-zinc-400">Matuê, Teto, Kayblack</span>
+          <h2 className="font-semibold text-2xl mt-10">
+            Made for Vinicius Viana
+          </h2>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10"
+            >
+              <img
+                src="/matue.webp"
+                className="w-full"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
+              <strong className="font-semibold">Daily mix 1</strong>
+              <span className="text-sm text-zinc-400">
+                Matuê, Teto, Kayblack
+              </span>
             </a>
-            <a href="#" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
-              <img src="/eminem.jpg" className="w-full" width={70} height={70}  alt="Album image"/>
-              <strong className="font-semibold ">Daily mix 2</strong>
-              <span className="Text-sm text-zinc-400">Eminem, Snoop Dogg, Akon</span>
+            <a
+              href="#"
+              className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10"
+            >
+              <img
+                src="/eminem.jpg"
+                className="w-full"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
+              <strong className="font-semibold">Daily mix 2</strong>
+              <span className="text-sm text-zinc-400">
+                Eminem, Snoop Dogg, Akon
+              </span>
             </a>
-            <a href="#" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
-              <img src="/tweeknd.jpg" className="w-full" width={70} height={70}  alt="Album image"/>
-              <strong className="font-semibold ">Daily mix 3</strong>
-              <span className="Text-sm text-zinc-400">The Weeknd, Travis Scott, Daft Punk</span>
+            <a
+              href="#"
+              className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10"
+            >
+              <img
+                src="/tweeknd.jpg"
+                className="w-full"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
+              <strong className="font-semibold">Daily mix 3</strong>
+              <span className="text-sm text-zinc-400">
+                The Weeknd, Travis Scott, Daft Punk
+              </span>
             </a>
-            <a href="#" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
-              <img src="/justin.png" className="w-full" width={70} height={70}  alt="Album image"/>
-              <strong className="font-semibold ">Daily mix 4</strong>
-              <span className="Text-sm text-zinc-400">Justin Bieber, Post Malone, The kid Laroi</span>
+            <a
+              href="#"
+              className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10"
+            >
+              <img
+                src="/justin.png"
+                className="w-full"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
+              <strong className="font-semibold">Daily mix 4</strong>
+              <span className="text-sm text-zinc-400">
+                Justin Bieber, Post Malone, The kid Laroi
+              </span>
             </a>
-            <a href="#" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
-              <img src="/eletronica.jpg" className="w-full" width={70} height={70}  alt="Album image"/>
-              <strong className="font-semibold ">Daily mix 5</strong>
-              <span className="Text-sm text-zinc-400">Eletronica</span>
+            <a
+              href="#"
+              className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10"
+            >
+              <img
+                src="/eletronica.jpg"
+                className="w-full"
+                width={70}
+                height={70}
+                alt="Album image"
+              />
+              <strong className="font-semibold">Daily mix 5</strong>
+              <span className="text-sm text-zinc-400">Eletronica</span>
             </a>
           </div>
         </main>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
